6900e6fe5ac3ac76e4b435f8680ab548401af98d
Poncho <jplyles1212@gmail.com>
2020-11-20 15:52:09 -0800

    Finish pseudocode for game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,12 @@
 
-# Set up game initially
-  # Create the board
-  # Create 2 players
-# Start game loop
-  # Render board
-  # 
+# Set up game initially [TicTacToe]
+  # Create the board [Board]
+  # Create 2 players [Player]
+# Start game loop [TicTacToe]
+  # Render board [Board]
+  # Ask for player's coordinates [Player]
+  # If game ends [TicTacToe]
+    # Display victory/draw message
+    # Stop loop
+  # Else
+    # Switch to nest player and keep looping [TicTacToe]
